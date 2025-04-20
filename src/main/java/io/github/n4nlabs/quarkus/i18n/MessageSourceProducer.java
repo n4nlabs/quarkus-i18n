9ddd247bcf7f86dba2827bae9a5b5ff52d2cf695
nferreira1 <nathan.1402@hotmail.com>
2025-04-20 18:57:55 -0300

    refactor: refactor MessageSourceProducer to use ConfigProvider instead of @ConfigProperty

--- src/main/java/io/github/n4nlabs/quarkus/i18n/MessageSourceProducer.java
+++ src/main/java/io/github/n4nlabs/quarkus/i18n/MessageSourceProducer.java
@@ -2,19 +2,20 @@ package io.github.n4nlabs.quarkus.i18n;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.config.Config;
 
 @ApplicationScoped
 public class MessageSourceProducer {
 
-    @ConfigProperty(name = "n4nlabs.quarkus-i18n.folder", defaultValue = "i18n")
-    String folder;
-
-    @ConfigProperty(name = "n4nlabs.quarkus-i18n.basename", defaultValue = "messages")
-    String basename;
-
     @Produces
     public MessageSource messageSource() {
+        Config config = ConfigProvider.getConfig();
+        String folder = config.getOptionalValue("n4nlabs.i18n.folder", String.class).orElse("i18n");
+        String basename = config.getOptionalValue("n4nlabs.i18n.basename", String.class).orElse("messages");
+
+        System.out.println(">> MessageSourceProducer folder=" + folder + " basename=" + basename);
         return new DefaultMessageSource(folder, basename);
     }
 
